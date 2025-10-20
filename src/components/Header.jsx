86a3772759a815444e8a026c2d6af06155862e10
RIRony9898 <rirony59@gmail.com>
2025-10-20 22:10:18 +0600

    add date-fns dependency and update Header component to display current date

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from '../assets/logo.png'
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
-        <div className='flex flex-col justify-center items-center'>
+        <div className='flex flex-col justify-center items-center gap-4'>
             <img src={logo} alt="" />
-            <p>Journalism Without Fear or Favour</p>
+            <p className='text-accent text-lg'>Journalism Without Fear or Favour</p>
+            <p className='text-accent text-xl font-medium'>{format(new Date(), 'EEEE, LLLL dd, yyyy')}</p>
         </div>
     );
 };
